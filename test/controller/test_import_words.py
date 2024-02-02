caf16014f6274a82bd37f79613cb388d065fe32d
Matheus Sousa <matheus.sousa.dev@gmail.com>
2024-02-02 00:14:30 -0400

    test: ensure ImportWordsController returns a error response when request has no fields

--- test/controller/test_import_words.py
+++ test/controller/test_import_words.py
@@ -1,4 +1,5 @@
 from src.controller.import_words import ImportWordsController
+import src.model.http.request as module_request
 
 import src.util.constant as module_constant
 
@@ -7,4 +8,11 @@ def test_request_is_null():
     response = sut.handle(None)
     assert response.success == False
     assert response.message == module_constant.DEFAULT_MESSAGE_RESPONSE_NULL_REQUEST
+    assert response.data == None
+
+def test_request_has_no_fields():
+    sut = ImportWordsController()
+    response = sut.handle(request=module_request.Request())
+    assert response.success == False
+    assert response.message == module_constant.DEFAULT_MESSAGE_RESPONSE_NO_FIELDS
     assert response.data == None
