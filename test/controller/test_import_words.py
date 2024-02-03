d85913c819ee3fd4d9450a83a072b1eba1dd0b8a
Matheus Sousa <matheus.sousa.dev@gmail.com>
2024-02-03 01:32:14 -0400

    test: ensure ImportWordsController handles exception when business throws

--- test/controller/test_import_words.py
+++ test/controller/test_import_words.py
@@ -3,6 +3,7 @@ import unittest.mock as mock
 import src.entities.model.http.request as module_request
 import src.entities.model.controller.import_words as module_controller_import_words
 import src.entities.adapter.import_words as module_adapter_import_words
+import src.business.import_words as module_business_import_words
 import src.util.helper.response as module_helper_response
 class SutTypes:
     def __init__(self, sut:ImportWordsController, adapter:module_adapter_import_words.ImportWordsAdapter):
@@ -11,7 +12,7 @@ class SutTypes:
 
 def get_sut_types(
         adapter:module_adapter_import_words.ImportWordsAdapter=module_adapter_import_words.ImportWordsAdapter(),
-        business=None
+        business=module_business_import_words.ImportWordsBusiness
     ):
     sut = ImportWordsController(adapter=adapter,business=business)
     return SutTypes(sut=sut, adapter=adapter)
@@ -59,3 +60,14 @@ def test_check_import_word_business_is_beign_called():
     request = module_request.Request(body={'file_dir':'any_file_dir'})
     sut.handle(request=request)
     import_words_business.import_words.assert_called_once_with(expected)
+
+def test_handle_exception_when_import_word_business_throws():
+    adapter = mock.MagicMock()
+    business = mock.MagicMock()
+    adapter.return_value.adapt.return_value = module_controller_import_words.ImportWordsRequest(file_dir='any_file_dir')
+    business.import_words.side_effect = Exception('any_error')
+    sut_types = get_sut_types(adapter=adapter, business=business)
+    sut = sut_types.sut
+    request = module_request.Request(body={'file_dir':'any_file_dir'})
+    response = sut.handle(request=request)
+    assert response == module_helper_response.generic_error_response()
