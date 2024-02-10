50344f5421f1db38326bb97c407c84dbc5a414cd
Matheus Sousa <matheus.sousa.dev@gmail.com>
2024-02-10 00:40:50 -0400

    chore: add assert in unit tests

--- tests/test_import_words_usecase.py
+++ tests/test_import_words_usecase.py
@@ -42,7 +42,7 @@ class TestImportWordUsecase(module_unittest.TestCase):
         request = module_model_import_words.ImportWordsRequest(file_dir="any_file_dir")
         with self.assertRaises(module_model_exception.AppException) as context:
             sut.import_words(request)
-        context.exception.message == module_constant.DEFAULT_EXCEPTION_DIRECTORY_NOT_FOUND
+        assert context.exception.message == module_constant.DEFAULT_EXCEPTION_DIRECTORY_NOT_FOUND
     
     def test_throw_app_error_when_no_files_moved(self):
         file_service = module_unittest_mock.MagicMock()
@@ -53,19 +53,18 @@ class TestImportWordUsecase(module_unittest.TestCase):
         request = module_model_import_words.ImportWordsRequest(file_dir="any_file_dir")
         with self.assertRaises(module_model_exception.AppException) as context:
             sut.import_words(request)
-        context.exception.message == module_constant.DEFAULT_EXCEPTION_NO_FILE_MOVED
+        assert context.exception.message == module_constant.DEFAULT_EXCEPTION_NO_FILE_MOVED
 
     def test_throw_error_when_could_not_read_file(self):
         file_service = module_unittest_mock.MagicMock()
         word_service = module_unittest_mock.MagicMock()
-        file_service.move_to_processing.return_value = ['any_file1', 'any_file2']
-        file_service.read_words_file.side_effect = Exception('any_error')
+        file_service.move_to_processing.return_value = []
         sut_types = get_sut_types(file_service=file_service, word_service=word_service)
         sut = sut_types.sut
         request = module_model_import_words.ImportWordsRequest(file_dir="any_file_dir")
         with self.assertRaises(module_model_exception.AppException) as context:
             sut.import_words(request)
-        context.exception.message == module_constant.DEFAULT_EXCEPTION_NO_FILE_MOVED
+        assert context.exception.message == module_constant.DEFAULT_EXCEPTION_NO_FILE_MOVED
     
     def test_check_word_service_is_beign_called(self):
         file_service = module_unittest_mock.MagicMock()
@@ -89,7 +88,7 @@ class TestImportWordUsecase(module_unittest.TestCase):
         request = module_model_import_words.ImportWordsRequest(file_dir="any_file_dir")
         with self.assertRaises(module_model_exception.AppException) as context:
             sut.import_words(request)
-        context.exception.message == module_constant.DEFAULT_EXCEPTION_ERROR_PERSISTING_WORDS
+        assert context.exception.message == module_constant.DEFAULT_EXCEPTION_ERROR_PERSISTING_WORDS
 
     def test_throw_app_error_when_moving_files_to_processed(self):
         file_service = module_unittest_mock.MagicMock()
@@ -102,4 +101,4 @@ class TestImportWordUsecase(module_unittest.TestCase):
         request = module_model_import_words.ImportWordsRequest(file_dir="any_file_dir")
         with self.assertRaises(module_model_exception.AppException) as context:
             sut.import_words(request)
-        context.exception.message == module_constant.DEFAULT_EXCEPTION_NO_FILE_MOVED
+        assert context.exception.message == module_constant.DEFAULT_EXCEPTION_NO_FILE_MOVED
